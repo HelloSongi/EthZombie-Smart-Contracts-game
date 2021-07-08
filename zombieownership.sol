0ebc5a65853163b12b1a3398fa6e5fd86da54240
HelloSongi <69304233+HelloSongi@users.noreply.github.com>
2021-07-08 23:32:37 +0300

    Update zombieownership.sol

--- zombieownership.sol
+++ zombieownership.sol
@@ -1,7 +1,7 @@
 pragma solidity >=0.5.0 <0.6.0;
 
 import "./zombieattack.sol";
-import "./erc721.sol";
+import "./erc271.sol";
 import "./safemath.sol";
 
 contract ZombieOwnership is ZombieAttack, ERC721 {
